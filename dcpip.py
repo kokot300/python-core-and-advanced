6c880fd02d4f46b0a42ec24bdc6726ab0261f222
kokot300 <kokot300@gmail.com>
2020-03-30 11:34:47 +0200

    yo!

--- dcpip.py
+++ dcpip.py
@@ -1,10 +1,16 @@
 import socket
 
-host='localhost'
-port=4000
+host = 'localhost'
+port = 4000
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET is using IP v4 #TCP IP SOCK_STEAM
-s.bind((host,port))
-s.listen(1)
-c,addr=s.accept()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET is using IP v4 #TCP IP SOCK_STEAM
+s.bind((host, port))
+print("listening from port ", port)
+s.listen(port)
+print("listening")
+print("yo",s.accept())
+addr = s.accept()
 print("connection from: ", str(addr))
+c.send(b"hello")  # converts into binary
+print("message sent")
+c.close()
